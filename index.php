8ce2dffcb4e8fe9aad5925b90cd5018e33838b36
Rümeysa Akbaş <rrumeysa.akbass@gmail.com>
2023-05-18 00:21:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="tr">
 <head>
@@ -12,16 +11,14 @@
     <div class="container">
         <div class="row">
             <form action="process.php?process=login" method="post" class="position-absolute top-50 start-50 translate-middle " style="width:310px; height:275px;">
-                <input class="mb-2 rounded text-center"  type="text" name="name" value="" placeholder="Adınız" required />
+                <input class="mb-2 rounded text-center"  type="email" name="email" value="" placeholder="Mail Adresiniz" required />
                 <input class="mb-2 rounded text-center" type="password" name="password" value="" placeholder="Şifreniz" required />
                 <button class="mb-2 rounded text-center" value="kayit" name="button" class="">Giriş Yap</button> <br>
-                <a class="text-decoration-none text-black" style="margin-left:70px;" href="sign_up.php">Kayıt olmak için tıklayın</a>
+                <a class="text-decoration-none text-black" style="margin-left:70px; color:black;" href="sign_up.php">Kayıt olmak için tıklayın</a>
             </form>
         </div>
     </div>
-
     <script src="bootstrap/bootstrap.js"></script>
-
 </body>
 </html>
 
